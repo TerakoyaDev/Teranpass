9d33879745ddb2e08b2ab86a6940f371b232f3cb
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-05-18 00:01:24 +0900

    feat(front): add front app
    create front app by create react app

---
+++ front/src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    // Render the Calendar
+    return (
+      <div>
+        hello
+      </div>
+    );
+  }
+}
+
+export default App;
