469475f969517a2919987c5fec2085e6c09cb5fd
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-05-18 00:04:33 +0900

    feat(firebase): add firebase config

---
+++ front/src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyBloXvTEZtwgLErQzVPHjYMxznQUcC_qt0",
+  authDomain: "teranpass.firebaseapp.com",
+  databaseURL: "https://teranpass.firebaseio.com",
+  messagingSenderId: "620880932898",
+  projectId: "teranpass",
+  storageBucket: "teranpass.appspot.com",
+};
