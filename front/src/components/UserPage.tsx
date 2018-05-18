0b0da641f377b40dfd0ce3fbe034d255e540b696
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-05-19 05:45:22 +0900

    feat(firebaseDB): add get function of firebase

--- front/src/components/UserPage.tsx
+++ front/src/components/UserPage.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {firebaseDb} from '../firebase'
 
 interface InterfaceProps {
   match: {
@@ -10,10 +11,20 @@ interface InterfaceProps {
 
 // TODO get userInfo by id
 export default class UserPage extends React.Component<InterfaceProps> {
+  constructor(props: InterfaceProps) {
+    super(props)
+  }
+
+  public async getUserInfo(id: string) {
+    const val = ((await firebaseDb.ref(`users/${id}`).once('value')).val())
+    console.log(val)
+    return val.displayName
+  }
+
   public render() {
     return (
       <div>
-        {this.props.match.params.id}
+        {this.getUserInfo(this.props.match.params.id)}
       </div>
     );
   }
