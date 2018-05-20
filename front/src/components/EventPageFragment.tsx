d9a713ce31cb0bab5e5affec05d269a8c95e37a0
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-05-20 20:53:27 +0900

    feat(event): Eventの参加，不参加処理を追加した

--- front/src/components/EventPageFragment.tsx
+++ front/src/components/EventPageFragment.tsx
@@ -5,10 +5,11 @@ import CardHeader from '@material-ui/core/CardHeader';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import Person from '@material-ui/icons/Person';
 import Subheader from 'material-ui/Subheader';
+import SvgIcon from 'material-ui/SvgIcon';
 import * as React from 'react';
 import { IUserInfo } from '../App';
+import { firebaseAuth, firebaseDb } from '../firebase';
 
 interface InterfaceProps {
   history: {
@@ -28,17 +29,118 @@ interface InterfaceProps {
     };
     title: string;
   };
+  getEvents: () => void;
+}
+
+interface InterfaceState {
+  isJoin: boolean;
 }
 
 // TODO get userInfo by id
-export default class EventPageFragment extends React.Component<InterfaceProps> {
+export default class EventPageFragment extends React.Component<
+  InterfaceProps,
+  InterfaceState
+> {
   constructor(props: InterfaceProps) {
     super(props);
+    this.state = { isJoin: false };
+
+    this.joinUserToEvent = this.joinUserToEvent.bind(this);
+    this.removeUserInEvent = this.removeUserInEvent.bind(this);
+    this.setJoin = this.setJoin.bind(this);
   }
+
+  public isAuthedAccount() {
+    const user = firebaseAuth.currentUser;
+    if (user) {
+      return this.props.event.sponsor.uid === user.uid;
+    }
+    return false;
+  }
+
+  public setJoin() {
+    const user = firebaseAuth.currentUser;
+    if (user) {
+      if (this.props.event.participants.find(n => n.uid === user.uid)) {
+        this.setState({ isJoin: true });
+      } else {
+        this.setState({ isJoin: false });
+      }
+    }
+  }
+
+  // join
+  public joinUserToEvent() {
+    const user = firebaseAuth.currentUser;
+    if (user) {
+      const ref = firebaseDb.ref(
+        `events/${this.props.event.date.split(' ')[0]}/${
+          this.props.event.eventId
+        }/participants`
+      );
+      ref.transaction(array => {
+        if (array) {
+          array.push({
+            displayName: user.displayName,
+            email: user.email,
+            photoURL: user.photoURL,
+            uid: user.uid,
+          });
+          return array;
+        } else {
+          return [];
+        }
+      });
+      this.setState({ isJoin: true });
+      this.props.getEvents();
+    }
+  }
+
+  // remove
+  public removeUserInEvent() {
+    const user = firebaseAuth.currentUser;
+    if (user) {
+      const ref = firebaseDb.ref(
+        `events/${this.props.event.date.split(' ')[0]}/${
+          this.props.event.eventId
+        }/participants`
+      );
+      ref.transaction(array => {
+        if (array) {
+          return array.filter((n: IUserInfo) => n.uid !== user.uid);
+        } else {
+          return [];
+        }
+      });
+      this.setState({ isJoin: false });
+      this.props.getEvents();
+    }
+  }
+
+  public deleteEvent() {}
 
   public pushUserPage(userInfo: IUserInfo) {
     this.props.history.push(`/users/${userInfo.uid}`);
   }
+
+  public componentDidMount() {
+    this.setJoin();
+  }
+
+  public joinButton = () =>
+    this.state.isJoin ? (
+      <IconButton onClick={this.removeUserInEvent}>
+        <SvgIcon>
+          <path d="M19,13H5V11H19V13Z" />
+        </SvgIcon>
+      </IconButton>
+    ) : (
+      <IconButton onClick={this.joinUserToEvent}>
+        <SvgIcon>
+          <path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
+        </SvgIcon>
+      </IconButton>
+    );
 
   // TODO porfile card and register event
   public render() {
@@ -46,14 +148,19 @@ export default class EventPageFragment extends React.Component<InterfaceProps> {
       <div>
         <Card>
           <CardHeader
-            avatar={
-              this.props.event.sponsor.photoURL !== 'default' ? (
-                <Avatar src={this.props.event.sponsor.photoURL} />
-              ) : (
-                <Avatar>
-                  <Person />
-                </Avatar>
-              )
+            avatar={<Avatar src={this.props.event.sponsor.photoURL} />}
+            action={
+              <div>
+                {this.isAuthedAccount() ? (
+                  <IconButton onClick={this.deleteEvent.bind(this)}>
+                    <SvgIcon>
+                      <path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
+                    </SvgIcon>
+                  </IconButton>
+                ) : (
+                  <this.joinButton />
+                )}
+              </div>
             }
             title={this.props.event.title}
             subheader={this.props.event.date}
@@ -71,7 +178,7 @@ export default class EventPageFragment extends React.Component<InterfaceProps> {
             <Subheader>{`このイベントに登録中のユーザ`}</Subheader>
             <Grid container={true} spacing={24}>
               {this.props.event.participants.map((val, index) => (
-                <Grid item={true} xs={1} key={index}>
+                <Grid item={true} xs={2} key={index}>
                   <IconButton onClick={this.pushUserPage.bind(this, val)}>
                     <Avatar src={val.photoURL} />
                   </IconButton>
